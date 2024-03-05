af812b1b4dd52f9b4e87e38321dd309f6f7ebcc6
B00143361 Amos Fakrogha <B00143361@mytudublin.ie>
2024-03-05 12:48:41 +0000

    step 4 completed

--- app/Http/Controllers/scorderController.php
+++ app/Http/Controllers/scorderController.php
@@ -9,10 +9,10 @@ use App\Http\Controllers\AppBaseController;
 use Illuminate\Http\Request;
 use Flash;
 use Response;
+use Session;
 
 class scorderController extends AppBaseController
 {
-    /** @var scorderRepository $scorderRepository*/
     private $scorderRepository;
 
     public function __construct(scorderRepository $scorderRepo)
@@ -20,38 +20,18 @@ class scorderController extends AppBaseController
         $this->scorderRepository = $scorderRepo;
     }
 
-    /**
-     * Display a listing of the scorder.
-     *
-     * @param Request $request
-     *
-     * @return Response
-     */
     public function index(Request $request)
     {
         $scorders = $this->scorderRepository->all();
 
-        return view('scorders.index')
-            ->with('scorders', $scorders);
+        return view('scorders.index')->with('scorders', $scorders);
     }
 
-    /**
-     * Show the form for creating a new scorder.
-     *
-     * @return Response
-     */
     public function create()
     {
         return view('scorders.create');
     }
 
-    /**
-     * Store a newly created scorder in storage.
-     *
-     * @param CreatescorderRequest $request
-     *
-     * @return Response
-     */
     public function store(CreatescorderRequest $request)
     {
         $input = $request->all();
@@ -63,13 +43,6 @@ class scorderController extends AppBaseController
         return redirect(route('scorders.index'));
     }
 
-    /**
-     * Display the specified scorder.
-     *
-     * @param int $id
-     *
-     * @return Response
-     */
     public function show($id)
     {
         $scorder = $this->scorderRepository->find($id);
@@ -83,13 +56,6 @@ class scorderController extends AppBaseController
         return view('scorders.show')->with('scorder', $scorder);
     }
 
-    /**
-     * Show the form for editing the specified scorder.
-     *
-     * @param int $id
-     *
-     * @return Response
-     */
     public function edit($id)
     {
         $scorder = $this->scorderRepository->find($id);
@@ -103,14 +69,6 @@ class scorderController extends AppBaseController
         return view('scorders.edit')->with('scorder', $scorder);
     }
 
-    /**
-     * Update the specified scorder in storage.
-     *
-     * @param int $id
-     * @param UpdatescorderRequest $request
-     *
-     * @return Response
-     */
     public function update($id, UpdatescorderRequest $request)
     {
         $scorder = $this->scorderRepository->find($id);
@@ -128,15 +86,6 @@ class scorderController extends AppBaseController
         return redirect(route('scorders.index'));
     }
 
-    /**
-     * Remove the specified scorder from storage.
-     *
-     * @param int $id
-     *
-     * @throws \Exception
-     *
-     * @return Response
-     */
     public function destroy($id)
     {
         $scorder = $this->scorderRepository->find($id);
@@ -152,5 +101,43 @@ class scorderController extends AppBaseController
         Flash::success('Scorder deleted successfully.');
 
         return redirect(route('scorders.index'));
+    }
+
+    public function checkout()
+    {
+        if (Session::has('cart')) {
+            $cart = Session::get('cart');
+            $lineitems = array();
+            foreach ($cart as $productid => $qty) {
+                $lineitem['product'] = \App\Models\Product::find($productid);
+                $lineitem['qty'] = $qty;
+                $lineitems[] = $lineitem;
+            }
+            return view('scorders.checkout')->with('lineitems', $lineitems);
+        }
+        else {
+            Flash::error("There are no items in your cart");
+            return redirect(route('products.displaygrid'));
+        }
+    }
+
+    public function placeorder(Request $request)
+    {
+        $thisOrder = new \App\Models\Scorder();
+        $thisOrder->orderdate = (new \DateTime())->format("Y-m-d H:i:s");
+        $thisOrder->save();
+        $orderID = $thisOrder->id;
+        $productids = $request->productid;
+        $quantities = $request->quantity;
+        for($i=0;$i<sizeof($productids);$i++) {
+            $thisOrderDetail = new \App\Models\OrderDetail();
+            $thisOrderDetail->orderid = $orderID;
+            $thisOrderDetail->productid = $productids[$i];
+            $thisOrderDetail->quantity = $quantities[$i];
+            $thisOrderDetail->save();
+        }
+        Session::forget('cart');
+        Flash::success("Your Order has Been Placed");
+        return redirect(route('products.displaygrid'));
     }
 }
